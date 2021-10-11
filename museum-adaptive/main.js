3d429778049bb3ad04579f29a09d4705c8e96c5d
RuslanPanasuk <panasuk.eetp@gmail.com>
2021-10-11 07:16:41 +0300

    fix: add form

--- museum-adaptive/main.js
+++ museum-adaptive/main.js
@@ -6,27 +6,14 @@ window.onload = function () {
     loop: true,
     items: 1,
     slideBy: 'page',
-    nav: false,
-    // navContainer: '.controls-dots',    
+    nav: false,   
     autoplay: false,
     speed: 400,
     autoplayButtonOutput: false,
     mouseDrag: true,
     lazyload: true,
-    controlsContainer: ".controls-arrows",  
-    // responsive: {
-    //   640: {
-    //     edgePadding: 20,
-    //     gutter: 20,
-    //     items: 2
-    //   },
-    //   700: {
-    //     gutter: 30
-    //   },
-    //   900: {
-    //     items: 3
-    //   }
-    // }
+    controlsContainer: ".controls-arrows", 
+    
   });
 
   const slider2 = tns({  
@@ -49,10 +36,6 @@ window.onload = function () {
     },
   });
 
-
-// ---------------
-
-
   // const progress_video = document.querySelector('.progress-video');
 
   // progress_video.addEventListener('input', function () {
@@ -67,25 +50,27 @@ window.onload = function () {
   //   this.style.background = `linear-gradient(to right, #710707; 0%, #710707; ${value}%, #c4c4c4 ${value}%, #c4c4c4 100%)`
   // })
 
-  // const buyNow = document.querySelector('.buy-now');
-  // const ticketsOverlay = document.querySelector('.tickets-overlay');
-  // const ticketsForm = document.querySelector('.tickets-form');
-  // const closeForm = document.querySelector('.close-form');
+  const buyNow = document.querySelector('.tickets-btn');
+  const ticketsOverlay = document.querySelector('.shadowed');
+  const ticketsForm = document.querySelector('.booking-tickets');
+  const closeForm = document.querySelector('.booking-tickets__btn-close');
 
-  // buyNow.addEventListener('click', function () {
-  //   ticketsOverlay.style.left = 0;
-  // })
+  buyNow.addEventListener('click', function () {
+    ticketsOverlay.style.display = 'block';
+    ticketsForm.style.display = 'flex';
+  })
 
 
-  // closeForm.addEventListener('click', function () {
-  //   ticketsOverlay.style.left = '-300%';
-  // })
+  closeForm.addEventListener('click', function () {
+    ticketsOverlay.style.display = 'none';
+    ticketsForm.style.display = 'none';
+  })
+  
   // window.addEventListener("click", (e) => {
   //   if (e.target == ticketsOverlay)
-  //     ticketsOverlay.style.left = '-300%';
+  //     ticketsOverlay.style.display = 'none';
+  //     ticketsForm.style.display = 'none';
   // });
-
-
 
 
   const burger = document.querySelector('.humburger-icon');
@@ -93,7 +78,6 @@ window.onload = function () {
   const navClose = document.querySelector('.icon-close');
   const welcomeCard = document.querySelector('.welcome-card');
   const navigator = document.querySelector('.menu-nav');
-
 
   burger.addEventListener('click', function () {
     openBurgerMenu();
@@ -112,11 +96,13 @@ window.onload = function () {
     navClose.style.display = 'none';
     welcomeCard.style.display = 'block';
   }
+
   function openBurgerMenu() {
     burger.style.display = 'none';
     menu.style.display = 'block';
     navClose.style.display = 'block';
     welcomeCard.style.display = 'none';
   }
+
 }
 
